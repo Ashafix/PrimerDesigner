4a67f09c551327c785c24b4206feab6cdb3450e3
Ashafix <maximili.peters@mail.huji.ac.il>
2018-08-01 15:09:02 -0400

    more API endpoints

--- python/FlaskJob.py
+++ python/FlaskJob.py
@@ -1,7 +1,10 @@
 import subprocess
 import sys
+import io
 import os
 import yaml
+import uuid
+from Bio.Blast import NCBIXML
 
 
 class FlaskJob:
@@ -19,10 +22,11 @@ class FlaskJob:
                     'output': self.stdout, 'stderr': self.stderr})
 
     def __repr__(self):
-        return 'FlaskJob(status={}, error={}, finished={}, future={}'.format(self.status,
-                                                                             self.error,
-                                                                             self.finished,
-                                                                             self.future)
+        return '{}(status={}, error={}, finished={}, future={}'.format(self.__class__,
+                                                                       self.status,
+                                                                       self.error,
+                                                                       self.finished,
+                                                                       self.future)
 
 
 class BlastJob(FlaskJob):
@@ -30,9 +34,11 @@ class BlastJob(FlaskJob):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.blast_executable = ''
-        self.db_directory = ''
-        self.query_directory = ''
+        self.directory_db = ''
+        self.directory_tmp = '/tmp/'
+        self.directory_query = ''
         self.get_locations()
+
 
     def get_locations(self):
 
@@ -44,16 +50,17 @@ class BlastJob(FlaskJob):
         else:
             config = {'blast': os.environ.get('BLAST_EXECUTABLE'),
                       'blast_db': os.environ.get('BLAST_DATABASE')}
-        print(config, file=sys.stderr)
         self.blast_executable = config.get('blast')
-        self.db_directory = config.get('blast_db')
-        if self.blast_executable is None or self.db_directory is None:
+        self.directory_db = config.get('blast_db')
+        if self.blast_executable is None or self.directory_db is None:
             raise ValueError('Blast executable and database dir need to be present '
                              'in config file or environment variable')
-        self.query_directory = os.path.join(self.db_directory, '..', 'query')
-        if not os.path.isdir(self.query_directory):
-            os.makedirs(self.query_directory)
-        return self.blast_executable, self.db_directory
+        if self.directory_query is None or len(self.directory_query) == 0:
+            self.directory_query = os.path.join(self.directory_db, '..', 'query')
+
+        if not os.path.isdir(self.directory_query):
+            os.makedirs(self.directory_query)
+        return self.blast_executable, self.directory_db
 
     def run(self, parameters=None):
         if parameters is None:
@@ -61,9 +68,14 @@ class BlastJob(FlaskJob):
         job_id = parameters.get('job_id', 'job_id')
         num_threads = parameters.get('num_threads', 4)
         output_format = parameters.get('outfmt', 5)
+        filename_query = '{}/{}.fa'.format(self.directory_db, job_id)
+        seq = seq_from_fasta(parameters['sequence'])
+
+        with open(filename_query, 'w') as f:
+            f.write(seq)
         call = [self.blast_executable,
-                '-db', '{}/nt'.format(self.db_directory),
-                '-query', '{}/{}.fa'.format(self.db_directory, job_id),
+                '-db', '{}/nt'.format(self.directory_db),
+                '-query', filename_query,
                 '-num_threads', str(num_threads),
                 '-outfmt', str(output_format)]
         proc = subprocess.Popen(call,
@@ -74,8 +86,52 @@ class BlastJob(FlaskJob):
         self.stdout = self.stdout.decode('utf-8')
         self.stderr = self.stderr.decode('utf-8')
 
-        print(type(self.stdout), file=sys.stderr)
         if self.stderr is None or len(self.stderr) > 0:
             self.error = True
         self.finished = True
         self.status = 'finished'
+
+    def get_accession(self, accession):
+
+        filename = None
+        while filename is None or os.path.exists(filename):
+            filename = os.path.join(self.directory_tmp, "blastdbcmd_{}".format(get_job_id()))
+        accession = '\n'.join(accession.split(';'))
+        with open(filename, 'w') as f:
+            f.write(accession)
+        call = [self.blast_executable.replace('blastn', 'blastdbcmd'),
+                '-db', '{}/nt'.format(self.directory_db),
+                '-entry_batch', filename]
+        proc = subprocess.Popen(call,
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE,
+                                universal_newlines=True)
+        stdout, stderr = proc.communicate()
+        if stderr is None or len(stderr) > 0:
+            return {'error': stderr}
+        return stdout
+
+
+def get_job_id():
+    return uuid.uuid4().hex[0:8]
+
+
+def seq_from_fasta(seq):
+    return seq
+    seq = seq.strip()
+    lines = seq.splitlines()
+    if lines[0].startswith('>'):
+        start = 1
+    else:
+        start = 0
+    return ''.join(seq[start:])
+
+
+def extract_hits_from_blast(blast_xml):
+    if isinstance(blast_xml, str):
+        blast_xml = io.StringIO(blast_xml)
+    blast_records = NCBIXML.read(blast_xml)
+    hits = []
+    for alignment in blast_records.alignments:
+        hits.append(alignment.accession)
+    return hits
--- python/ServerPrimerDesigner.py
+++ python/ServerPrimerDesigner.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restful import Resource, Api
 from flask_restful import reqparse
+from flask import jsonify
 import subprocess
 import sys
 import sqlite3
@@ -9,61 +10,102 @@ import uuid
 import concurrent.futures
 import functools
 import uuid
-from FlaskJob import BlastJob
+from FlaskJob import BlastJob, get_job_id, extract_hits_from_blast
+import json
+import flask
+
 
 app = Flask(__name__)
 api = Api(app)
 jobs = dict()
 executor = concurrent.futures.ThreadPoolExecutor(10)
 
+parser = reqparse.RequestParser()
+parser.add_argument('accession', action='append')
+parser.add_argument('format', required=False, default='txt', choices=['txt', 'json'])
 
-class RestBlast(Resource):
+
+class RestBlastMinimal(Resource):
+
     def get(self, blast_id):
-        if blast_id is None:
-            return 'noooone'
+        if blast_id is None or len(blast_id) == 0:
+            return flask.abort(404)
         if blast_id in jobs:
             return str(jobs[blast_id].stdout)
 
         return [str(j) for j in jobs.values()]
 
-    def post(self, blast_id):
+class RestBlast(Resource):
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('blast_id', type=str, help='BLAST ID')
+        args = parser.parse_args()
+        return RestNucleotideMinimal().get(args['blast_id'])
+
+    def post(self):
         parser = reqparse.RequestParser()
         parser.add_argument('sequence', type=str, help='Sequence')
         args = parser.parse_args()
-        #conn = sqlite3.connect('blast_jobs.db')
-        #c = conn.cursor()
 
-        job_id = uuid.uuid4().hex
-        cmd = ("INSERT INTO jobs VALUES ('{}', '{}', '{}', '{}', '{}')".format(job_id, args['sequence'], 0000, time.time(), 'submitted'))
-        #with open('job_id.fa', 'w') as f:
-        #    f.write('>{}\n{}'.format('bla', args['sequence']))
-        #with open('f.txt', 'w') as f:
-        #    f.write(cmd)
-            #c.execute(cmd)
-            
-            #call = ['/media/ashafix/WesternDigital/ncbi-blast-2.7.1+/bin/blastn', '-db', '/media/ashafix/WesternDigital/db/nt', '-query', '/media/ashafix/WesternDigital/job_id.fa', '>>', job_id + 'txt']
-            #f.write('\n'.join(call))
-            #subprocess.run(' '.join(call), shell=True)
         job_id = get_job_id()
-        jobs[job_id] = BlastJob()
-        print('starting', file=sys.stderr)
-        jobs[job_id].future = executor.submit(jobs[job_id].run)
-        print('started', file=sys.stderr)
+        cmd = ("INSERT INTO jobs VALUES ('{}', '{}', '{}', '{}', '{}')".format(job_id, args['sequence'], 0000, time.time(), 'submitted'))
+
+        conn = sqlite3.connect('blast_jobs.db')
+        c = conn.cursor()
+        c.execute(cmd)
+        conn.commit()
+        conn.close()
+        job = BlastJob()
+        args['job_id'] = job_id
+        job.future = executor.submit(functools.partial(job.run, parameters=args))
+        jobs[job_id] = job
 
         return job_id
-        #conn.commit()
-        #conn.close()
+
 
 class RestNucleotide(Resource):
     def get(self):
-        return {'id': 'values'}
+        args = parser.parse_args()
+        args['accession'] = args['accession'][0]
+        if args['format'] == 'txt':
+            return RestNucleotideMinimal().get(args['accession'])
+        else:
+            job = BlastJob()
+            acc = job.get_accession(args['accession'])
+            return jsonify({'response': acc})
 
-def get_job_id():
-    return uuid.uuid4().hex[0:8]
+    def post(self):
+        args = parser.parse_args()
+        job = BlastJob()
+        resp = {}
+        for acc in args['accession']:
+            resp[acc] = job.get_accession(acc)
+        if args['format'] == 'txt':
+            return resp
+        else:
+            return jsonify(resp)
 
 
-api.add_resource(RestBlast, '/blast/<blast_id>')
-api.add_resource(RestNucleotide, '/nucleotide')
+class RestNucleotideMinimal(Resource):
+    def get(self, accession):
+        job = BlastJob()
+        acc = job.get_accession(accession)
+        return flask.Response(acc, mimetype='txt')
+
+
+class RestBlastHits(Resource):
+    def get(self, blast_id):
+        if blast_id is None or len(blast_id) == 0 or blast_id not in jobs:
+            return flask.abort(404)
+        if not jobs[blast_id].finished:
+            return flask.abort(404)
+        return extract_hits_from_blast(jobs[blast_id].stdout)
+
+api.add_resource(RestBlast, '/blast/')
+api.add_resource(RestBlastMinimal, '/blast/<blast_id>')
+api.add_resource(RestBlastHits, '/blast/hits/<blast_id>')
+api.add_resource(RestNucleotide, '/nucleotide/')
+api.add_resource(RestNucleotideMinimal, '/nucleotide/<accession>')
 
 
 if __name__ == '__main__':
